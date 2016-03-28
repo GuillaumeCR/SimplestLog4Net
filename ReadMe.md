ad5e44f9f82b125632c834215a31ce7b095e2e90
GuillaumeCR <guillaume.cretotrichert@mckesson.ca>
2016-03-28 12:47:20 -0700

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+# SimplestLog4Net
+This is the simplest functional meaningful log4net integration. Build on this.
+# Why?
+I could never find a "Get Started" article that didn't have at least 1 or 2 things that needed to be tweaked before log4net worked. This is the simplest template I could think of.
---
+++ SimplestLog4Net/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SimplestLog4Net
+{
+    using log4net;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var errors = log4net.Config.XmlConfigurator.Configure();
+            var log = LogManager.GetLogger("");
+            log.Debug("Simple!");
+        }
+    }
+}
